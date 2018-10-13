fc02e40dc5e215103afb992acb5763de2daf8fe3
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-10-13 11:05:43 -0700

    Center game board

---
+++ react-client/src/components/Board.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Board extends Component {
+  render() {
+    return (
+      <div className='board'></div>
+    );
+  }
+}
+
+Board.propTypes = {
+  // TBD
+};
+
+export default Board;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Triangle from './components/Triangle.jsx'
+import Board from './components/Board.jsx'
 import $ from 'jquery';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    }
+    this.state = {};
   }
 
-  componentDidMount() {
-    // TBD
+  createVerticesTraingle(length, svgSize) {
+    let vertices = [];
+    vertices.push([(svgSize / 2) - (length / 2), svgSize]);
+    vertices.push([svgSize / 2, svgSize - (length * Math.sqrt(3) / 2)]);
+    vertices.push([(svgSize / 2) + (length / 2), svgSize])
+    return vertices;
   }
 
   render() {
     return (
       <div>
-        <div>App</div>
+        {/* <svg width={1000} height={1000}>
+          <Triangle vertices={this.createVerticesTraingle(500, 200)} color="#FF8EAC" delay={0} />
+        </svg> */}
+        <Board></Board>
       </div>
-    )
+
+    );
   }
 }
 
